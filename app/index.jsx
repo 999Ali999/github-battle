00d2fbda3f3120d2dc1cef6409deb791b332ebad
999Ali999 <999alishafiei999@gmail.com>
2025-01-07 17:56:19 +0100

    initialize project structure with essential components and configurations

---
+++ app/index.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Battle from "./components/Battle";
+import Nav from "./components/Nav";
+import Popular from "./components/Popular";
+import Results from "./components/Results";
+import "./index.css";
+
+class App extends React.Component {
+  state = {
+    theme: "light",
+  };
+
+  toggleTheme = () => {
+    this.setState(({ theme }) => ({
+      theme: theme === "light" ? "dark" : "light",
+    }));
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav theme={this.state.theme} toggleTheme={this.toggleTheme} />
+            <Routes>
+              <Route path="/" element={<Popular />} />
+              <Route path="/battle" element={<Battle />} />
+              <Route path="/results" element={<Results />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+const rootElement = document.getElementById("app");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
